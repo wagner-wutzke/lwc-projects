07ab178fe05a2cf72b08fb72549aa2338315b962
Wagner O. Wutzke <wagner.wutzke@google.com>
2026-07-08 18:31:48 -0300

    configured jest for test coverage

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,21 @@
-const { jestConfig } = require('@salesforce/sfdx-lwc-jest/config');
+const { jestConfig } = require("@salesforce/sfdx-lwc-jest/config");
 
 module.exports = {
-    ...jestConfig,
-    modulePathIgnorePatterns: ['<rootDir>/.localdevserver']
+  ...jestConfig,
+  // Define files Jest should collect coverage from
+  collectCoverageFrom: [
+    "force-app/main/default/lwc/**/*.js",
+    "!**/__tests__/**"
+  ],
+  // Enforce minimum threshold targets (fails the build if unmet)
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  },
+  // Choose specific output report formats
+  coverageReporters: ["clover", "json", "text", "lcov", "cobertura"]
 };
