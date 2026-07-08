d580ee4e85632720e0dbfca117f05168c24cdc1a
Wagner O. Wutzke <wagner.wutzke@google.com>
2026-07-08 06:37:49 -0300

    moved project to root project folder

---
+++ force-app/main/default/lwc/filter/filter.js
@@ -0,0 +1,64 @@
+import { LightningElement, wire } from "lwc";
+import getAllProjectsForFilter from "@salesforce/apex/TimeTrackingLogController.getAllProjects";
+
+export default class Filter extends LightningElement {
+  allProjects = undefined;
+
+  startDateInitValue;
+  endDateInitValue;
+  selectedProjectId;
+
+  connectedCallback() {
+    const now = new Date();
+    this.startDateInitValue = new Date(now.getFullYear(), 1 /*now.getMonth()*/, 1).toISOString().split('T')[0];
+    this.endDateInitValue = new Date(now.getFullYear(), now.getMonth() + 1, 0).toISOString().split('T')[0];
+    this.selectedProjectId = 'All';
+  }
+
+  @wire(getAllProjectsForFilter)
+  wiredProjectRecords({ error, data }) {
+    if (data) {
+      this.generateProjectFilterOptions(data);
+    } else if (error) {
+      console.error("Error loading project records: ", error);
+    }
+  }
+
+  generateProjectFilterOptions(data) {
+    // Build options array with a default "All" option
+    this.allProjects = [
+      { label: "All", value: "All" },
+      ...Array.from(data).map((project) => ({
+        label: project.Name,
+        value: project.Id
+      }))
+    ];
+  }
+
+  handleProjectChange(event) {
+    const selectedProjectId = event.detail.value;
+    const projectChangeEvent = new CustomEvent("projectchange", {
+      detail: selectedProjectId,
+      bubbles: true
+    });
+    this.dispatchEvent(projectChangeEvent);
+  }
+
+  handleStartDateChange(event) {
+    const startDate = event.detail.value;
+    const startDateChangeEvent = new CustomEvent("startdatechange", {
+      detail: startDate,
+      bubbles: true
+    });
+    this.dispatchEvent(startDateChangeEvent);
+  }
+
+  handleEndDateChange(event) {
+    const endDate = event.detail.value;
+    const endDateChangeEvent = new CustomEvent("enddatechange", {
+      detail: endDate,
+      bubbles: true
+    });
+    this.dispatchEvent(endDateChangeEvent);
+  }
+}
